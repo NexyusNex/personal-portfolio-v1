b86fe8a77430da924c486db843f87c37693f9a34
NexyusNex <shootdietale@gmail.com>
2023-04-14 17:46:30 +0200

    Added content container with text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,14 @@
 import "../style/Home.css";
 
 export default function Home() {
-  return <div className="Home"></div>;
+  return (
+    <div className="Home">
+      <div className="home-content">
+        <h1>
+          Hello! I am <span>Nemanja Mitić!</span>
+        </h1>
+        <p>A Frontend developer</p>
+      </div>
+    </div>
+  );
 }
