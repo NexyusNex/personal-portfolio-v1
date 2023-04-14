f884372e36c408b9ea752b653b84c1cd0091d4b2
NexyusNex <shootdietale@gmail.com>
2023-04-14 21:39:48 +0200

    Wrapped personal info in div

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -12,15 +12,17 @@ import wp from "../Images/devexp/webpack-colored.svg";
 export default function Aboutme() {
   return (
     <div className="Aboutme">
-      <h1>About me</h1>
-      <h1>...</h1>
-      <img src={meImg} alt="me"></img>
-      <p>
-        I am a young fast learning and self motivated individual open to new
-        experiences and I am always looking for self improvement. Passionate
-        about everything that involves computers and programming and I regularly
-        engage in hobbies that involve them.
-      </p>
+      <div className="personal-info">
+        <h1>About me</h1>
+        <h1>...</h1>
+        <img src={meImg} alt="me"></img>
+        <p>
+          I am a young fast learning and self motivated individual open to new
+          experiences and I am always looking for self improvement. Passionate
+          about everything that involves computers and programming and I
+          regularly engage in hobbies that involve them.
+        </p>
+      </div>
       <div className="findme-links">
         <h2>You can find me here:</h2>
         <div className="findme-link-svg">
