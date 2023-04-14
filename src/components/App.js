3b7b930c5ec09ae905eb980421d273678b35f17d
NexyusNex <shootdietale@gmail.com>
2023-04-14 19:54:27 +0200

    Added aboutme component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import "../style/App.css";
+import Aboutme from "./Aboutme";
 import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <Home></Home>
+      <Aboutme></Aboutme>
     </div>
   );
 }
