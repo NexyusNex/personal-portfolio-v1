7321ea5411281b3a6dd6a0b17a9b1d3a9ff18d43
NexyusNex <shootdietale@gmail.com>
2023-04-15 19:53:35 +0200

    Added observer for animation

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "../style/App.css";
 import Aboutme from "./Aboutme";
 import Footer from "./Footer";
@@ -5,6 +6,20 @@ import Home from "./Home";
 import Navmenu from "./Navmenu";
 
 function App() {
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+    const hidden = document.querySelectorAll(".hidden");
+    hidden.forEach((el) => observer.observe(el));
+  }, []);
+
   return (
     <div className="App">
       <Home></Home>
