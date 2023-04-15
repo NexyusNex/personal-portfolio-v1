dd71ece80ad95c6a901c4f428d61ea59e795262c
NexyusNex <shootdietale@gmail.com>
2023-04-15 20:12:11 +0200

    No longer removing class show

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -18,8 +18,6 @@ export default function Aboutme() {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add("show");
-        } else {
-          entry.target.classList.remove("show");
         }
       });
     });
