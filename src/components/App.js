1c0de659e26e7dc1e989ae0a390b7cc155abac20
NexyusNex <shootdietale@gmail.com>
2023-04-15 20:02:33 +0200

    Removed observer

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import "../style/App.css";
 import Aboutme from "./Aboutme";
 import Footer from "./Footer";
@@ -6,20 +5,6 @@ import Home from "./Home";
 import Navmenu from "./Navmenu";
 
 function App() {
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-        } else {
-          entry.target.classList.remove("show");
-        }
-      });
-    });
-    const hidden = document.querySelectorAll(".hidden");
-    hidden.forEach((el) => observer.observe(el));
-  }, []);
-
   return (
     <div className="App">
       <Home></Home>
