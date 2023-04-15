ca39d90db89c2c42ca00285967651e515f0940df
NexyusNex <shootdietale@gmail.com>
2023-04-15 17:22:52 +0200

    Added svg

--- src/components/Navmenu.js
+++ src/components/Navmenu.js
@@ -3,6 +3,12 @@ import "../style/Navmenu.css";
 export default function Navmenu() {
   return (
     <div className="Nav-menu">
+      <button className="hamburger-button">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <title>menu</title>
+          <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+        </svg>
+      </button>
       <h1>Nemanja Mitić</h1>
       <p>About me</p>
       <p>Experience</p>
