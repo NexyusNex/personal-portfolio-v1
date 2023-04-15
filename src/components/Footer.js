c9aeef74fae70d19ffaeb8f622105e3033274ff8
NexyusNex <shootdietale@gmail.com>
2023-04-15 19:26:20 +0200

    Added footer waves

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import "../style/Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <div className="waves">
+        <div class="wave" id="wave1"></div>
+        <div class="wave" id="wave2"></div>
+        <div class="wave" id="wave3"></div>
+        <div class="wave" id="wave4"></div>
+      </div>
+    </div>
+  );
+}
