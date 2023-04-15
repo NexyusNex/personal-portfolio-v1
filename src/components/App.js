4c7d84c29f2add775deeed4038b729a8eb871db6
NexyusNex <shootdietale@gmail.com>
2023-04-15 16:52:01 +0200

    Added navmenu component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import "../style/App.css";
 import Aboutme from "./Aboutme";
 import Home from "./Home";
+import Navmenu from "./Navmenu";
 
 function App() {
   return (
     <div className="App">
       <Home></Home>
+      <Navmenu></Navmenu>
       <Aboutme></Aboutme>
     </div>
   );
