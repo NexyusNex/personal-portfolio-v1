0fc5cecc91ae9c81975f42c670d9b0912ff43911
NexyusNex <shootdietale@gmail.com>
2023-04-15 17:26:49 +0200

    Added function to toggle menu

--- src/components/Navmenu.js
+++ src/components/Navmenu.js
@@ -1,9 +1,13 @@
 import "../style/Navmenu.css";
 
 export default function Navmenu() {
+  function toggleMenu() {
+    document.querySelector(".Nav-menu").classList.toggle("animate");
+  }
+
   return (
     <div className="Nav-menu">
-      <button className="hamburger-button">
+      <button className="hamburger-button" onClick={toggleMenu}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <title>menu</title>
           <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
